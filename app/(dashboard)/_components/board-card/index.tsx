07ff3f74d99510253a8f4e60ceb442b9d3bd936b
Roukaya Chelly <roukayachelly7@gmail.com>
2024-06-28 00:23:19 +0100

    boards

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -1,6 +1,10 @@
 "use client";
+import { useAuth } from "@clerk/nextjs";
+import { Overlay } from "./overlay";
 import Image from "next/image";
 import Link from "next/link";
+import { formatDistanceToNow } from "date-fns";
+import { Footer } from "./footer";
 
 interface BoardCardProps {
   id: string;
@@ -23,18 +27,29 @@ export const BoardCard = ({
   orgId,
   isFavorite,
 }: BoardCardProps) => {
+  const { userId } = useAuth();
+  const authorLabel = userId === authorId ? "You" : authorName;
+  const createdAtLabel = formatDistanceToNow(createdAt, {
+    addSuffix: true,
+  });
+
   return (
-  <Link href={`/board/${id}`}>
-    <div className="group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden">
-    <div className="relative flex-1 bg-amber-50">
-        <Image
-        src={imageUrl}
-        alt="Doodle"
-        fill
-        className="object-fit"
+    <Link href={`/board/${id}`}>
+      <div className="group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden">
+        <div className="relative flex-1 bg-amber-50">
+          <Image src={imageUrl} alt={title} fill className="object-fit" />
+          <Overlay />
+        </div>
+
+        <Footer
+          isFavorite={isFavorite}
+          title={title}
+          authorLabel={authorLabel}
+          createdAtLabel={createdAtLabel}
+          onClick={() => {}}
+          disabled={false}
         />
-    </div>
-    </div>
-  </Link>
-);
+      </div>
+    </Link>
+  );
 };
