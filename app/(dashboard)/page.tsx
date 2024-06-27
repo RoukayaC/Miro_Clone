e9f4289b94d39b97accd3333ae1892b72463c63f
Roukaya Chelly <roukayachelly7@gmail.com>
2024-06-27 22:12:45 +0100

    boards

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import { useOrganization } from "@clerk/nextjs";
-import { EmptyOrg } from "./_components/emptyOrg";
-import { BoardList } from "./_components/boardList";
+import { EmptyOrg } from "./_components/empty-org";
+import { BoardList } from "./_components/board-list";
 
 interface DashboardPageProps {
   searchParams: {
     search?: string;
-    favourites?: string;
+    favorites?: string;
   };
 }
 
@@ -19,7 +19,10 @@ export default function DashboardPage({ searchParams }: DashboardPageProps) {
       {!organization ? (
         <EmptyOrg />
       ) : (
-        <BoardList orgId={organization.id} query={searchParams} />
+        <BoardList 
+        orgId={organization.id} 
+        query={searchParams} 
+        />
       )}
     </div>
   );
