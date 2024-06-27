167bf75457ba5f8a0a9ff963529765adea8ab384
Roukaya Chelly <roukayachelly7@gmail.com>
2024-06-27 13:01:42 +0100

    update

---
+++ app/(dashboard)/_components/search-input.tsx
@@ -0,0 +1,13 @@
+"use client";
+import qs from "query-string";
+import {Search} from "lucide-react";
+import {useDebounce} from "usehooks-ts";
+import {useRouter} from "next/navigation";
+
+export const SearchInput = () => {
+    return (
+        <div>
+            Search Input
+        </div>
+    );
+};
